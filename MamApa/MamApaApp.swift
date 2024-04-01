cd38abc111e0093fbe414e47907f510005eb2a0c
Diki Dwi Diro <dikidwid0@gmail.com>
2024-04-01 08:42:27 +0700

    Initial Commit

---
+++ MamApa/MamApaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MamApaApp.swift
+//  MamApa
+//
+//  Created by Diki Dwi Diro on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MamApaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
